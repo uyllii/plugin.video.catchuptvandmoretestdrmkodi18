418a26c708f6d6a4f7760f49149eb77a3f677214
wwark <wark@free.fr>
2018-11-15 19:45:40 +0100

    0.1.0~beta09 - Backport changes of French TV Guide from principal plugin

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -34,4 +34,3 @@ def get_item_media_path(item_media_path):
     full_path = os.path.join(
         Script.get_info("path"), "resources", "media", *(item_media_path))
     return ensure_native_str(full_path)
-
